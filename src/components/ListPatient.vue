3015307ad5819da919b91e3faa1b4164b3c0763a
María Jesús Fuenzalida <maria.fuenzalida.serrano@gmail.com>
2020-06-18 13:12:56 -0400

    Primer avance con despliegue de la lista de pacientes

---
+++ src/components/ListPatient.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <ul>
+        <li :key="patient.id" v-for="patient in patients">
+          {{patient.data.name}}
+          {{patient.data.email}}
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  methods: {
+    // 
+    ...mapActions(['setPatients'])
+  },
+  computed: {
+    ...mapState(['patients'])
+  },
+  created() {
+    this.setPatients()
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+const baseUrl = 'https://us-central1-hospital-36c8f.cloudfunctions.net/patients'
+
 export default new Vuex.Store({
   state: {
+    patients: []
   },
   mutations: {
+    // Va a agarrar los pacientes y los metera en la data
+    SET_PATIENTS(state, data) { state.patients = data }
   },
   actions: {
+    // 
+    setPatients({commit}) {
+      axios.get(`${baseUrl}/patients`)
+      .then((response) => {
+        commit('SET_PATIENTS', response.data)
+      })
+    }
   },
   modules: {
   }
